2174677182dfd0d3b345e52610e6ed8d0c1a9f85
Ivan Frantar <ivan.frantar@stelltec.com>
2021-12-13 23:15:39 +0000

    Updated form again.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,13 +1,13 @@
-document.querySelector("form").addEventListener("submit", handleSubmit);
-
-const handleSubmit = (e) => {
-  e.preventDefault()
-  let myForm = document.getElementById('requestDemo');
-  let formData = new FormData(myForm)
-  fetch('/', {
-    method: 'POST',
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString()
-  }).then(() => console.log('Form successfully submitted')).catch((error) =>
-    alert(error))
+if (document.querySelector("form").addEventListener("submit", handleSubmit)) {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let myForm = document.getElementById('requestDemo');
+    let formData = new FormData(myForm)
+    fetch('/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString()
+    }).then(() => console.log('Form successfully submitted')).catch((error) =>
+      alert(error))
+  }
 }
