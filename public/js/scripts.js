52d30c25acdcd4ede0362737d68653fd284d8da5
Ivan Frantar <ivan.frantar@stelltec.com>
2021-12-14 12:05:19 +0000

    Updates on the form code. Now I have it working.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,13 +1,15 @@
-document.querySelector("form").addEventListener("submit", handleSubmit);
-
-const handleSubmit = (e) => {
-  e.preventDefault()
-  let myForm = document.getElementById('pizzaOrder');
-  let formData = new FormData(myForm)
-  fetch('/', {
-    method: 'POST',
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString()
-  }).then(() => console.log('Form successfully submitted')).catch((error) =>
-    alert(error))
+let formElem = document.querySelector("form");
+console.log(formElem,"ivan");
+if (formElem.addEventListener("submit", handleSubmit)) {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let myForm = document.getElementById('requestDemo');
+    let formData = new FormData(myForm)
+    fetch('/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString()
+    }).then(() => console.log('Form successfully submitted')).catch((error) =>
+      alert(error))
+  }
 }
